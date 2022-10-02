92d9a68150b0950f557eb3b438fc3f6ae18fb3ba
gdim47 <gdim47@protonmail.com>
2022-10-02 21:21:36 +0300

    Separate cmd logic and fibonacci calculation

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+pub mod fib {
+    pub fn fibonacci_number(n: u32) -> u64 {
+        let GoldenRatio : f64 = (1. + 5.0_f64.sqrt()) / 2.;
+        return match n {
+            0 => 1,
+            _ => ((GoldenRatio.powi(n as i32) - (1. - GoldenRatio).powi(n as i32))
+                    / 5.0_f64.sqrt()) as u64
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
 use std::io;
 
-fn fibonacci_number(n: u32) -> u64 {
-    let GoldenRatio : f64 = (1. + 5.0_f64.sqrt()) / 2.;
-    return match n {
-        0 => 1,
-        _ => ((GoldenRatio.powi(n as i32) - (1. - GoldenRatio).powi(n as i32))
-                / 5.0_f64.sqrt()) as u64
-    }
-}
+extern crate fib;
+use fib::fib::fibonacci_number;
 
 fn main() {
     println!("Enter N for calculating N-th fibonacci number: ");
